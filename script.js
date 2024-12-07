c3e417db91329513cdeae68147dec469e1bc82a4
OyunUfosu <65866825+OyunUfosu@users.noreply.github.com>
2024-12-07 13:48:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const youtubeButton = document.getElementById('youtubeButton');
+
+youtubeButton.addEventListener('click', () => {
+    window.location.href = 'file:///C:/Users/halil/OneDrive/Masa%C3%BCst%C3%BC/projeler/Yeni%20klas%C3%B6r/Files/index.html';
+});
